49785a6d1b3b5df7cc778d86f3b924868f489ad1
Firsakov Dmitriy <firsakovds@mail.ru>
2023-02-05 00:14:26 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 // Находим форму в DOM
-let popUp = document.querySelector('.popup')
-let formElement = popUp.querySelector('.popup__form')
+const popUp = document.querySelector('.popup')
+const formElement = popUp.querySelector('.popup__form')
 // Находим поля формы в DOM
 let nameInput = formElement.querySelector('.popup__input_place_name') 
 let jobInput = formElement.querySelector('.popup__input_place_job')
-let savePopupBut = formElement.querySelector('.popup__save')
+const savePopupBut = formElement.querySelector('.popup__save')
 //находим блок profile
-let profile = document.querySelector('.profile')
+const profile = document.querySelector('.profile')
 //Находим кнопку для вызова popup
-let editBut = profile.querySelector('.profile__edit-button')
+const editBut = profile.querySelector('.profile__edit-button')
 //Находим поля блока profile c именем и профессией
 let nameHero = profile.querySelector('.profile__title')
 let profHero = profile.querySelector('.profile__subtitle')
 // выбираем необходимы элементы блока popup (кнопку закрытия popup)
-let popupCloseBut = popUp.querySelector('.popup__button-close_type_profile')
+const popupCloseBut = popUp.querySelector('.popup__button-close_type_profile')
 //описываем реакцию на КЛИК кнопок profile__edit-button и popup__button-close
 editBut.addEventListener('click', openPopup)
 popupCloseBut.addEventListener('click', closePopup)
@@ -35,7 +35,7 @@ function handleFormSubmit (evt) {
 formElement.addEventListener('submit', handleFormSubmit);
 // ниже идут задания пр5
 //popup формы добавления карточки
-const popUpAdd = document.querySelector('.popup__add-card');
+const popUpAdd = document.querySelector('.popup_type_add-card');
 //форма карточки в доме
 const formUpAdd = popUpAdd.querySelector('.popup__form-add');
 //кнопкаоткрытия попап карточки
@@ -43,8 +43,8 @@ const buttonAdd = document.querySelector('.profile__add-button');
 //кнопка закрытия попап карточки
 const buttonAddClose = popUpAdd.querySelector('.popup__button-close_type_add');
 //поля ввода попапа карточки
-const titleInput = formUpAdd.querySelector('.popup__input_place_space');
-const linkInput = formUpAdd.querySelector('.popup__input_place_link');
+let titleInput = formUpAdd.querySelector('.popup__input_place_space');
+let linkInput = formUpAdd.querySelector('.popup__input_place_link');
 //кнопка создать карточку
 const buttonCreateNewCard = formUpAdd.querySelector('.popup__create');
 //темплейт нового места
@@ -52,7 +52,7 @@ const cardTemplate = document.querySelector('#elements__card').content;
 const cardContainer = document.querySelector('.elements');
 // элементы попапа клозер
 //находим кнопку выхова попап картинки
-const popupImage = document.querySelector('.popup__image-closer');
+const popupImage = document.querySelector('.popup_type_image-closer');
 const popupFoto =  popupImage.querySelector('.popup__photo');
 const popupImageText = popupImage.querySelector('.popup__photo-text');
 const popupImageClose = popupImage.querySelector('.popup__button-close_type_image');
@@ -101,7 +101,7 @@ function createCard (title, link) {
   const cardElement = cardTemplate.cloneNode(true);
   //переменные поле и ссылка
   const cardFoto = cardElement.querySelector('.element__foto');
-  const cardTitle = cardElement.querySelector('.element__title');
+  let cardTitle = cardElement.querySelector('.element__title');
   const cardLink = cardFoto.src = link;
   const cardName = cardTitle.textContent = title;
   //реакция на клик по лайку
