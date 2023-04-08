f6a761f5ca3f4b76b4f90484ec2618c83fbbb676
Firsakov Dmitriy <firsakovds@mail.ru>
2023-04-08 23:59:07 +0300

    немного сотворил

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,20 +1,20 @@
 export default class UserInfo {
   constructor({nameHero, profHero}) {
     this._name = document.querySelector(nameHero);
-    this._prof = document.querySelector(profHero);
+    this._about = document.querySelector(profHero);
     //console.log(this._names)
   }
   //который возвращает объект с данными пользователя. 
   getUserInfo() {    
    return {
       name: this._name.textContent,
-      prof: this._prof.textContent,      
+      about: this._about.textContent,      
     }    
   }
   //который принимает 
   //новые данные пользователя и добавляет их на страницу.
   setUserInfo({inputName, inputJob}) {
     this._name.textContent = inputName;
-    this._prof.textContent = inputJob;    
+    this._about.textContent = inputJob;    
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Section from '../components/Section.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import UserInfo from '../components/UserInfo.js';
+import Api from '../components/Api.js';
 import {
   profile,
   popupEditProfile,
@@ -21,6 +22,21 @@ import {
   config,
   initialCards,
 } from '../utils/constants.js'
+
+const api = new Api({
+ baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-63',
+ headers: {
+    authorization: 'fd6fbc3f-6166-47d9-adb3-abe48f771ff4',
+    'Content-Type': 'application/json'
+  }
+});
+Promise.all([api.getUserData(), api.getInitialCards()])
+  .then(([userServerData, cardsData]) => {
+    userInfo.setUserInfo(userServerData);
+    cardList.renderItem(cardsData);
+  }).catch((err) => {
+    alert(err);
+  });
 //копии
 const userInfo = new UserInfo({
   nameHero: '.profile__title',
@@ -28,7 +44,7 @@ const userInfo = new UserInfo({
 })
 const cardList = new Section(
   {
-    items: initialCards,
+    items: [],//initialCards
     renderer:  createNewCard,
   },
   '.elements'
@@ -45,6 +61,7 @@ const popupEditForm = new PopupWithForm('.popup_type_edit-profile', handleFormPr
 popupEditForm.setEventListeners();
 const popupCardForm = new PopupWithForm('.popup_type_add-card', handleFormSublitCard)
 popupCardForm.setEventListeners();
+
 function createNewCard(dataCard) {
   const card = new Card(dataCard, '#elements__card', handleOpenPopup);  
   return card.createCard()
@@ -65,9 +82,9 @@ buttonAdd.addEventListener("click", () => {
   popupCardForm.open();
 })
 editBut.addEventListener("click", () => {
-  const {name, prof} = userInfo.getUserInfo();
+  const {name, about} = userInfo.getUserInfo();
   nameInput.value = name;
-  jobInput.value = prof;
+  jobInput.value = about;
   profValid.resetValidation();
   popupEditForm.open();
  })
